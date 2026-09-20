e63d051246fe9197d0c951e217b268be11242bb8
Serhii Slipets <slepecsergejj@gmail.com>
2026-09-20 15:33:13 +0200

    Add files via upload

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,46 @@
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-
-    changeUsername(newName) {
-        this.username = newName;},
-    updatePlayTime(hours) { this.playTime += hours; },
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
-    }
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+    .filter(user => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
 };
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+const clients = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
